79840b6b8a5573014b59e6722d5b38bbfac58a77
Kristian Mandrup <kmandrup@gmail.com>
2020-02-16 19:19:04 +0000

    base plan for dot

--- base.tf
+++ base.tf
@@ -1,10 +1,15 @@
 provider "aws" {
   access_key = ""
   secret_key = ""
-  region = "us-east-1"
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "base" {
-  ami = "ami-0d729a60"
+  ami           = "ami-0d729a60"
   instance_type = "t2.micro"
+}
+
+resource "aws_eip" "base" {
+  instance = aws_instance.base.id
+  vpc = true
 }
