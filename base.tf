9b4c8efb6f07d42b4772b74c68c903a632f677aa
Kristian Mandrup <kmandrup@gmail.com>
2020-02-16 19:25:34 +0000

    add and use variables

--- base.tf
+++ base.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
 
 resource "aws_instance" "base" {
-  ami           = "ami-0d729a60"
+  ami           = var.ami[var.region]
   instance_type = "t2.micro"
 }
 
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "access_key" {
+  type = string
+  description = "AWS access key"
+}
+
+variable "secret_key" {
+  type = string
+  description = "AWS secret key"
+}
+
+variable "region" {
+  type = string
+  description = "us-east-1"
+}
+
+variable "region_list" {
+  type = list(string)
+  description = "AWS availability zones"
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "ami" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-0d729a60"
+    us-west-1 = "ami-7c4b331c"
+  }
+  description = "The AMIs to use"
+}
